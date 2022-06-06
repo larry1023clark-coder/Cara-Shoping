294fb2839cf04fe61a60561ba0232446d9b65b45
larry1023clark-coder <larry1023clark@gmail.com>
2022-06-07 01:46:13 +0200

    add items to cart

--- assist/JS/main.js
+++ assist/JS/main.js
@@ -42,7 +42,6 @@ function darkMode(){
     document.querySelector('.input-group input').classList.toggle('darkForm');
     
     
-
     // change logo image
     var logoImg = document.getElementById('brandLogo');
     var bodyName = document.querySelector('body').classList ;
@@ -53,9 +52,113 @@ function darkMode(){
     }
 }
 
+// -----------------------------------------------------------
+let cartIcon = document.querySelectorAll('.fa-cart-shopping')
+
+
+let divAddItems = document.querySelector('.cartAddItems');
+
+let productImg = document.querySelectorAll('.pCard .oImg')
+let productType = document.querySelectorAll('.card-title')
+let productPrice = document.querySelectorAll('.price')
+
+
+let quantaty = document.querySelectorAll('.quantaty') ;
+let totalItem = document.querySelector('#subTotalItem')
+let totalPrice = document.getElementById('subTotalPrice')
+
+let removeItemBtn = document.querySelectorAll('.removeItem')
+
+let cartItemeProduct = document.querySelectorAll('cartProduct')
+
+let itemOrItems = document.querySelector('.itemOrItems')
+
+let cartNVar = 1
+let totalItemVar = 1
+let totalPriceVar = 0.00
+let cartN = document.querySelector('.cartN')
+
+// cartN.innerHTML = cartNVar ;
+
+
+
+cartIcon.forEach(function(car){
+    car.addEventListener('click', function(bb){
+
+        divAddItems.innerHTML += '<div class="cartProduct">' + '<h4 class="removeItem" title="Remove Item">x</h4>' + '<img src="' 
+        + bb.target.dataset.src + '" width="75px">' + '<h5>' + bb.target.dataset.type + '</h5>' 
+        + '<h5>' + '$' + bb.target.dataset.price + '</h5>' 
+        + '<h5>' + '<span class="decrease">- &nbsp;</span>' 
+        + '<span class="quantaty">1</span>'  +'<span class="increase" title="Increase quantaty">&nbsp; +</span>' + '</h5>' + '</div>'
+      
+        cartN.innerHTML = cartNVar++
+        totalItem.innerHTML = totalItemVar++
+        
+        totalPriceVar = totalPriceVar + Number(bb.target.dataset.price) ;
+                                        // parseInt  it's remove desimal .99
+        totalPrice.innerHTML = totalPriceVar.toFixed(2) 
+
+        itemOrItemsFun()
+
+
+
+    })
+})
+
+
+
+removeItemBtn.forEach(function(remove){
+    remove.addEventListener('click', function(item){
+        item.style.display = "none"
+        cartN.innerHTML = cartN - 1
+
+        cartNVar = cartNVar--
+        cartN.innerHTML = cartNVar
+        totalItem.innerHTML = totalItemVar--
+
+        totalPriceVar = totalPriceVar - Number(bb.target.dataset.price) ;
+        totalPrice.innerHTML = totalPriceVar.toFixed(2) 
+    })
+
+})
+
+function itemOrItemsFun(){
+
+if(totalItemVar <= 1 ){
+    itemOrItems.innerHTML = "item"
+}else{
+    itemOrItems.innerHTML = "items"
+}}
+
+
+let decrease = document.querySelector('#decrease') ;
+let increase = document.querySelector('#increase') ;
 
 
 
 
 
 
+
+// function decreaseItem(){
+//     if (quantaty >= 1){
+//         quantaty = quantaty - 1;
+//     }else{
+
+// }   }
+
+// decrease.addEventListener('click', decreaseItem)
+
+
+
+increase
+document.addEventListener('click', function(inc){
+    if(inc.target.className === "increase"){
+        quantaty = quantaty++;
+    }
+})
+
+
+
+// decrease.addEventListener('click', increaseQun)
+// increase.addEventListener('click', increaseQun)
